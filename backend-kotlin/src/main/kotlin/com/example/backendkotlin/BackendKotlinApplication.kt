ea4d89e7862c9f2cfa08935603a09e6a20e66618
pokotsun <gorigolila@gmail.com>
2024-03-22 01:29:15 +0900

    spring boot initializr で環境構築

---
+++ backend-kotlin/src/main/kotlin/com/example/backendkotlin/BackendKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.backendkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendKotlinApplication>(*args)
+}
---
+++ backend-kotlin/src/test/kotlin/com/example/backendkotlin/BackendKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.backendkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
