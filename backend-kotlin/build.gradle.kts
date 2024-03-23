3f412d5b0f798eb453c67069dc034de9d807c701
pokotsun <gorigolila@gmail.com>
2024-03-23 11:11:56 +0900

    flyway の mysql に変更

--- backend-kotlin/build.gradle.kts
+++ backend-kotlin/build.gradle.kts
@@ -34,8 +34,9 @@ sourceSets {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j:8.3.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
