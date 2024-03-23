c987d8af1f353a5f88aeebe7fd8b3328a4447704
pokotsun <gorigolila@gmail.com>
2024-03-23 11:33:06 +0900

    ./gradlew spotlessApply

--- backend-kotlin/src/main/kotlin/com/example/backendkotlin/BackendKotlinApplication.kt
+++ backend-kotlin/src/main/kotlin/com/example/backendkotlin/BackendKotlinApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class BackendKotlinApplication
 
 fun main(args: Array<String>) {
-	runApplication<BackendKotlinApplication>(*args)
+    runApplication<BackendKotlinApplication>(*args)
 }
