5ae1eff03434e97b65348425d31bd5e4a82e9910
pokotsun <gorigolila@gmail.com>
2024-04-29 21:34:13 +0900

    main 修正

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -10,12 +10,14 @@ function Home() {
     return (
         <div className="main-content background-image" style={{ backgroundImage: `url(${backgroundImage})` }}>
             <h1 className="title">人狼オンライン</h1>
-            <button onClick={() => navigate("/create-village")}>
-                村を作る
-            </button>
-            <button onClick={() => navigate("/enter-village")}>
-                村に入る
-            </button>
+            <div className="card">
+                <button onClick={() => navigate("/create-village")}>
+                    村を作る
+                </button>
+                <button onClick={() => navigate("/enter-village")}>
+                    村に入る
+                </button>
+            </div>
         </div>
     )
 }
