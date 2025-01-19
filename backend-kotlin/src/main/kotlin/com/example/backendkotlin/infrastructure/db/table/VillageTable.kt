94c80b308bedc7c99b394ebba67a4980d1451939
soiloof <tsucchy0718@gmail.com>
2025-01-19 09:17:16 +0900

    villageのpasswordをsalt&hashの組み合わせで保持するように修正

--- backend-kotlin/src/main/kotlin/com/example/backendkotlin/infrastructure/db/table/VillageTable.kt
+++ backend-kotlin/src/main/kotlin/com/example/backendkotlin/infrastructure/db/table/VillageTable.kt
@@ -6,13 +6,14 @@ import org.jetbrains.exposed.sql.kotlin.datetime.datetime
 
 object VillageTable : UUIDTable("village") {
     val name = varchar("name", length = 255)
-    val password = varchar("password", length = 255)
+    val salt = varchar("salt", length = 255)
+    val passwordHash = varchar("password_hash", length = 255)
     val citizenCount = integer("citizen_count")
     val werewolfCount = integer("werewolf_count")
     val fortuneTellerCount = integer("fortune_teller_count")
     val knightCount = integer("knight_count")
     val psychicCount = integer("psychic_count")
-    val madmanCount = integer("madman_count",)
+    val madmanCount = integer("madman_count")
     val isInitialActionActive = bool("is_initial_action_active")
     val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
     val updatedAt = datetime("updated_at").defaultExpression(CurrentDateTime)
