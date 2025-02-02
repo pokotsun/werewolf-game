d71c90003a8ea468b7da88ad022c3e0fe0e4e13d
soiloof <tsucchy0718@gmail.com>
2025-02-02 10:09:51 +0900

    BCryptの利用のためspring-starter-securityを追加

--- backend-kotlin/build.gradle.kts
+++ backend-kotlin/build.gradle.kts
@@ -49,6 +49,7 @@ dependencyManagement {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("org.postgresql:postgresql:42.7.3")
