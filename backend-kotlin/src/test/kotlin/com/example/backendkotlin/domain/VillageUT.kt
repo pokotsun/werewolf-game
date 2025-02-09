0129d805a4db3f2a15acbd96e150e455082c22d3
soiloof <tsucchy0718@gmail.com>
2025-02-09 09:29:05 +0900

    VillageUTでgiven条件を追加する書き方に修正

--- backend-kotlin/src/test/kotlin/com/example/backendkotlin/domain/VillageUT.kt
+++ backend-kotlin/src/test/kotlin/com/example/backendkotlin/domain/VillageUT.kt
@@ -1,60 +1,96 @@
 package com.example.backendkotlin.domain
 
+import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.assertDoesNotThrow
 import java.util.UUID
 
 class VillageUT : DescribeSpec() {
     init {
         describe("Village") {
             it("全ての値が条件を満たしているので正常に初期化できる") {
-                assertDoesNotThrow {
+                // given
+                val id = VillageId(UUID.randomUUID())
+                val name = "Village 1"
+                val citizenCount = 10
+                val werewolfCount = 2
+                val fortuneTellerCount = 1
+                val knightCount = 1
+                val psychicCount = 1
+                val madmanCount = 1
+                val isInitialActionActive = true
+
+                // when, then
+                shouldNotThrowAny {
                     Village(
-                        id = VillageId(UUID.randomUUID()),
-                        name = "Village 1",
-                        citizenCount = 10,
-                        werewolfCount = 2,
-                        fortuneTellerCount = 1,
-                        knightCount = 1,
-                        psychicCount = 1,
-                        madmanCount = 1,
-                        isInitialActionActive = true,
+                        id = id,
+                        name = name,
+                        citizenCount = citizenCount,
+                        werewolfCount = werewolfCount,
+                        fortuneTellerCount = fortuneTellerCount,
+                        knightCount = knightCount,
+                        psychicCount = psychicCount,
+                        madmanCount = madmanCount,
+                        isInitialActionActive = isInitialActionActive,
                     )
                 }
             }
             it("userNumberが全役職の数の合計になっている") {
                 // given
+                val id = VillageId(UUID.randomUUID())
+                val name = "Village 1"
+                val citizenCount = 10
+                val werewolfCount = 2
+                val fortuneTellerCount = 1
+                val knightCount = 1
+                val psychicCount = 1
+                val madmanCount = 1
+                val isInitialActionActive = true
+                val expectedUserNumber = citizenCount + werewolfCount + fortuneTellerCount + knightCount + psychicCount + madmanCount
+
+                // when
                 val village = Village(
-                    id = VillageId(UUID.randomUUID()),
-                    name = "Village 1",
-                    citizenCount = 10,
-                    werewolfCount = 2,
-                    fortuneTellerCount = 1,
-                    knightCount = 1,
-                    psychicCount = 1,
-                    madmanCount = 1,
-                    isInitialActionActive = true,
+                    id = id,
+                    name = name,
+                    citizenCount = citizenCount,
+                    werewolfCount = werewolfCount,
+                    fortuneTellerCount = fortuneTellerCount,
+                    knightCount = knightCount,
+                    psychicCount = psychicCount,
+                    madmanCount = madmanCount,
+                    isInitialActionActive = isInitialActionActive,
                 )
+                val actual = village.userNumber
 
                 // then
-                village.userNumber shouldBe 16
+                actual shouldBe expectedUserNumber
             }
 
             it("市民の数が0未満の場合は例外が発生する") {
+                // given
+                val id = VillageId(UUID.randomUUID())
+                val name = "Village 1"
+                val citizenCount = -1
+                val werewolfCount = 2
+                val fortuneTellerCount = 1
+                val knightCount = 1
+                val psychicCount = 1
+                val madmanCount = 1
+                val isInitialActionActive = true
+
                 // when
                 val exception = shouldThrow<IllegalArgumentException> {
                     Village(
-                        id = VillageId(UUID.randomUUID()),
-                        name = "Village 1",
-                        citizenCount = -1,
-                        werewolfCount = 2,
-                        fortuneTellerCount = 1,
-                        knightCount = 1,
-                        psychicCount = 1,
-                        madmanCount = 1,
-                        isInitialActionActive = true,
+                        id = id,
+                        name = name,
+                        citizenCount = citizenCount,
+                        werewolfCount = werewolfCount,
+                        fortuneTellerCount = fortuneTellerCount,
+                        knightCount = knightCount,
+                        psychicCount = psychicCount,
+                        madmanCount = madmanCount,
+                        isInitialActionActive = isInitialActionActive,
                     )
                 }
 
@@ -62,18 +98,29 @@ class VillageUT : DescribeSpec() {
                 exception.message shouldBe "citizenCount must be greater than or equal to 0"
             }
             it("複数のメンバ変数が条件を満たしていなくても例外が発生し先の方の例外がスローされる") {
+                // given
+                val id = VillageId(UUID.randomUUID())
+                val name = "Village 1"
+                val citizenCount = 10
+                val werewolfCount = -1
+                val fortuneTellerCount = -100000
+                val knightCount = 1
+                val psychicCount = 1
+                val madmanCount = 1
+                val isInitialActionActive = true
+
                 // when
                 val exception = shouldThrow<IllegalArgumentException> {
                     Village(
-                        id = VillageId(UUID.randomUUID()),
-                        name = "Village 1",
-                        citizenCount = 10,
-                        werewolfCount = -1,
-                        fortuneTellerCount = -100000,
-                        knightCount = 1,
-                        psychicCount = 1,
-                        madmanCount = 1,
-                        isInitialActionActive = true,
+                        id = id,
+                        name = name,
+                        citizenCount = citizenCount,
+                        werewolfCount = werewolfCount,
+                        fortuneTellerCount = fortuneTellerCount,
+                        knightCount = knightCount,
+                        psychicCount = psychicCount,
+                        madmanCount = madmanCount,
+                        isInitialActionActive = isInitialActionActive,
                     )
                 }
 
