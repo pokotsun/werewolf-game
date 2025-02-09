839aea7957f85a711f80c84960c2157dc802d0be
soiloof <tsucchy0718@gmail.com>
2025-02-09 17:43:23 +0900

    利用していないREST API向けの実装を削除

--- backend-kotlin/src/main/kotlin/com/example/backendkotlin/presentation/response/HelloResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.backendkotlin.presentation.response
-
-import java.util.*
-
-data class HelloResponse(val value: String, val users: User?) {
-    data class User(val userId: UUID, val name: String)
-}
