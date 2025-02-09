1fac0abc615c8c55d56fcbfe50cd6e4e701f7cf0
soiloof <tsucchy0718@gmail.com>
2025-02-09 10:51:22 +0900

    TestDatabaseの設定処理をkotestのProjectListenerとして統合

--- backend-kotlin/src/test/kotlin/com/example/backendkotlin/config/WerewolfGameProjectListener.kt
+++ backend-kotlin/src/test/kotlin/com/example/backendkotlin/config/WerewolfGameProjectListener.kt
@@ -3,13 +3,18 @@ package com.example.backendkotlin.config
 import com.example.backendkotlin.infrastructure.db.table.RUserVillageTable
 import com.example.backendkotlin.infrastructure.db.table.UserTable
 import com.example.backendkotlin.infrastructure.db.table.VillageTable
+import io.kotest.core.annotation.AutoScan
 import io.kotest.core.listeners.BeforeProjectListener
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.testcontainers.containers.PostgreSQLContainer
 
-object InitializeTestDatabase : BeforeProjectListener {
+/**
+ * プロジェクト実行前に共通で実行されるべき処理をまとめたクラス
+ */
+@AutoScan
+object WerewolfGameProjectListener : BeforeProjectListener {
     private const val POSTGRESQL_IMAGE = "postgres:14.11"
     private val testContainer = PostgreSQLContainer<Nothing>(POSTGRESQL_IMAGE).apply {
         withDatabaseName("werewolf")
