8b360b651ae9a7dc98a4a4a6b2a5e04ee0ed3b2c
soiloof <tsucchy0718@gmail.com>
2025-02-11 00:31:33 +0900

    RUserVillageRepositoryの実装にレビューを反映

--- backend-kotlin/src/main/kotlin/com/example/backendkotlin/infrastructure/db/RUserVillageRepositoryImpl.kt
+++ backend-kotlin/src/main/kotlin/com/example/backendkotlin/infrastructure/db/RUserVillageRepositoryImpl.kt
@@ -4,7 +4,6 @@ import com.example.backendkotlin.domain.RUserVillageRepository
 import com.example.backendkotlin.domain.UserId
 import com.example.backendkotlin.domain.VillageId
 import com.example.backendkotlin.infrastructure.db.table.RUserVillageTable
-import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.stereotype.Repository
@@ -18,15 +17,11 @@ class RUserVillageRepositoryImpl() : RUserVillageRepository {
      * {@inheritDoc}
      */
     override fun save(userId: UserId, villageId: VillageId): Pair<UserId, VillageId> {
-        try {
-            transaction {
-                RUserVillageTable.insert {
-                    it[RUserVillageTable.userId] = userId.value
-                    it[RUserVillageTable.villageId] = villageId.value
-                }
+        transaction {
+            RUserVillageTable.insert {
+                it[RUserVillageTable.userId] = userId.value
+                it[RUserVillageTable.villageId] = villageId.value
             }
-        } catch (e: ExposedSQLException) {
-            throw RuntimeException("Failed to create r_user_village", e)
         }
         return Pair(userId, villageId)
     }
--- backend-kotlin/src/test/kotlin/com/example/backendkotlin/infrastracture/db/RUserRepositoryImplIT.kt
+++ backend-kotlin/src/test/kotlin/com/example/backendkotlin/infrastracture/db/RUserRepositoryImplIT.kt
@@ -9,12 +9,15 @@ import com.example.backendkotlin.infrastructure.db.table.RUserVillageTable
 import com.example.backendkotlin.infrastructure.db.table.UserTable
 import com.example.backendkotlin.infrastructure.db.table.VillageTable
 import io.kotest.assertions.throwables.shouldNotThrowAny
-import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.core.spec.Spec
+import io.kotest.assertions.throwables.shouldThrowExactly
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
 import io.kotest.matchers.shouldBe
+import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.jetbrains.exposed.sql.deleteAll
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.boot.test.context.SpringBootTest
 import java.util.*
 
 /**
@@ -22,72 +25,65 @@ import java.util.*
  *
  * TestContainerを利用してDBの接続を行い、RUserRepositoryImplのテストを行う
  */
-class RUserRepositoryImplIT() : DescribeSpecUsingPostgreSQLTestContainer() {
-    private companion object {
-        const val EXIST_USER_ID_STRING = "00000000-0000-0000-0000-000000000000"
-        const val EXIST_VILLAGE_ID_STRING = "99999999-9999-9999-9999-999999999999"
-    }
-    private val rUserVillageRepository = RUserVillageRepositoryImpl()
-
-    // テスト前にUserTableとVillageTableに初期データを挿入する
-    override suspend fun beforeSpec(spec: Spec) {
-        super.beforeSpec(spec)
-        val user = User(
-            id = UserId(UUID.fromString(EXIST_USER_ID_STRING)),
-            name = "ユーザ1",
-            isActive = true,
-        )
-        val village = Village(
-            id = VillageId(UUID.fromString(EXIST_VILLAGE_ID_STRING)),
-            name = "村1",
-            citizenCount = 10,
-            werewolfCount = 2,
-            fortuneTellerCount = 1,
-            knightCount = 1,
-            psychicCount = 1,
-            madmanCount = 1,
-            isInitialActionActive = false,
-            gameMasterUserId = user.id,
-        )
-        val saltInput = "salt"
-        val hashedPassword = "hashedPassword"
+@SpringBootTest
+class RUserRepositoryImplIT(
+    private val rUserVillageRepository: RUserVillageRepositoryImpl,
+) : DescribeSpecUsingPostgreSQLTestContainer() {
+    // 全てのテスト後にUserTable, VillageTable, RUserVillageTableのデータを初期化する
+    override suspend fun afterTest(testCase: TestCase, result: TestResult) {
+        super.afterTest(testCase, result)
         transaction {
-            UserTable.insert {
-                it[id] = user.id.value
-                it[name] = user.name
-                it[isActive] = user.isActive
-            }
-            VillageTable.insert {
-                it[id] = village.id.value
-                it[name] = village.name
-                it[salt] = saltInput
-                it[passwordHash] = hashedPassword
-                it[citizenCount] = village.citizenCount
-                it[werewolfCount] = village.werewolfCount
-                it[fortuneTellerCount] = village.fortuneTellerCount
-                it[knightCount] = village.knightCount
-                it[psychicCount] = village.psychicCount
-                it[madmanCount] = village.madmanCount
-                it[isInitialActionActive] = village.isInitialActionActive
-                it[gameMasterUserId] = village.gameMasterUserId.value
-            }
+            UserTable.deleteAll()
+            VillageTable.deleteAll()
+            RUserVillageTable.deleteAll()
         }
     }
 
     init {
-        // 全てのテスト後にRUserVillageTableのデータを初期化する
-        afterTest {
-            transaction {
-                RUserVillageTable.deleteAll()
-            }
-        }
-
-        describe("save") {
+        this.describe("save") {
             context("正常系") {
                 it("ユーザが村に参加する") {
                     // given
-                    val userId = UserId(UUID.fromString(EXIST_USER_ID_STRING))
-                    val villageId = VillageId(UUID.fromString(EXIST_VILLAGE_ID_STRING))
+                    val userId = UserId(UUID.randomUUID())
+                    val user = User(userId, "user", true)
+                    val villageId = VillageId(UUID.randomUUID())
+                    val village = Village(
+                        id = villageId,
+                        name = "village",
+                        citizenCount = 10,
+                        werewolfCount = 2,
+                        fortuneTellerCount = 1,
+                        knightCount = 1,
+                        psychicCount = 1,
+                        madmanCount = 1,
+                        isInitialActionActive = false,
+                        gameMasterUserId = userId,
+                    )
+                    val saltInput = "salt"
+                    val hashedPassword = "hashedPassword"
+
+                    // and
+                    transaction {
+                        UserTable.insert {
+                            it[id] = userId.value
+                            it[name] = user.name
+                            it[isActive] = user.isActive
+                        }
+                        VillageTable.insert {
+                            it[id] = villageId.value
+                            it[name] = village.name
+                            it[salt] = saltInput
+                            it[passwordHash] = hashedPassword
+                            it[citizenCount] = village.citizenCount
+                            it[werewolfCount] = village.werewolfCount
+                            it[fortuneTellerCount] = village.fortuneTellerCount
+                            it[knightCount] = village.knightCount
+                            it[psychicCount] = village.psychicCount
+                            it[madmanCount] = village.madmanCount
+                            it[isInitialActionActive] = village.isInitialActionActive
+                            it[gameMasterUserId] = village.gameMasterUserId.value
+                        }
+                    }
 
                     // when
                     val result = rUserVillageRepository.save(userId, villageId)
@@ -99,45 +95,123 @@ class RUserRepositoryImplIT() : DescribeSpecUsingPostgreSQLTestContainer() {
             context("異常系") {
                 it("同じユーザが同じ村に参加すると例外が発生する") {
                     // given
-                    val userId = UserId(UUID.fromString(EXIST_USER_ID_STRING))
-                    val villageId = VillageId(UUID.fromString(EXIST_VILLAGE_ID_STRING))
+                    val userId = UserId(UUID.randomUUID())
+                    val user = User(userId, "user", true)
+                    val villageId = VillageId(UUID.randomUUID())
+                    val village = Village(
+                        id = villageId,
+                        name = "village",
+                        citizenCount = 10,
+                        werewolfCount = 2,
+                        fortuneTellerCount = 1,
+                        knightCount = 1,
+                        psychicCount = 1,
+                        madmanCount = 1,
+                        isInitialActionActive = false,
+                        gameMasterUserId = userId,
+                    )
+                    val saltInput = "salt"
+                    val hashedPassword = "hashedPassword"
+
+                    // and
+                    transaction {
+                        UserTable.insert {
+                            it[id] = userId.value
+                            it[name] = user.name
+                            it[isActive] = user.isActive
+                        }
+                        VillageTable.insert {
+                            it[id] = villageId.value
+                            it[name] = village.name
+                            it[salt] = saltInput
+                            it[passwordHash] = hashedPassword
+                            it[citizenCount] = village.citizenCount
+                            it[werewolfCount] = village.werewolfCount
+                            it[fortuneTellerCount] = village.fortuneTellerCount
+                            it[knightCount] = village.knightCount
+                            it[psychicCount] = village.psychicCount
+                            it[madmanCount] = village.madmanCount
+                            it[isInitialActionActive] = village.isInitialActionActive
+                            it[gameMasterUserId] = village.gameMasterUserId.value
+                        }
+                    }
 
                     // and
                     shouldNotThrowAny { rUserVillageRepository.save(userId, villageId) }
 
-                    // when
-                    val exception = shouldThrow<RuntimeException> {
+                    // when, then
+                    shouldThrowExactly<ExposedSQLException> {
                         rUserVillageRepository.save(userId, villageId)
                     }
-
-                    // then
-                    exception.message shouldBe "Failed to create r_user_village"
                 }
                 it("UserTableに存在しないユーザが村に参加すると例外が発生する") {
                     // given
                     val userId = UserId(UUID.randomUUID())
-                    val villageId = VillageId(UUID.fromString(EXIST_VILLAGE_ID_STRING))
+                    val user = User(userId, "user", true)
+                    val villageId = VillageId(UUID.randomUUID())
+                    val village = Village(
+                        id = villageId,
+                        name = "village",
+                        citizenCount = 10,
+                        werewolfCount = 2,
+                        fortuneTellerCount = 1,
+                        knightCount = 1,
+                        psychicCount = 1,
+                        madmanCount = 1,
+                        isInitialActionActive = false,
+                        gameMasterUserId = userId,
+                    )
+                    val saltInput = "salt"
+                    val hashedPassword = "hashedPassword"
+                    val anotherUserId = UserId(UUID.randomUUID())
 
-                    // when, then
-                    val exception = shouldThrow<RuntimeException> {
-                        rUserVillageRepository.save(userId, villageId)
+                    // and
+                    transaction {
+                        UserTable.insert {
+                            it[id] = userId.value
+                            it[name] = user.name
+                            it[isActive] = user.isActive
+                        }
+                        VillageTable.insert {
+                            it[id] = villageId.value
+                            it[name] = village.name
+                            it[salt] = saltInput
+                            it[passwordHash] = hashedPassword
+                            it[citizenCount] = village.citizenCount
+                            it[werewolfCount] = village.werewolfCount
+                            it[fortuneTellerCount] = village.fortuneTellerCount
+                            it[knightCount] = village.knightCount
+                            it[psychicCount] = village.psychicCount
+                            it[madmanCount] = village.madmanCount
+                            it[isInitialActionActive] = village.isInitialActionActive
+                            it[gameMasterUserId] = village.gameMasterUserId.value
+                        }
                     }
 
-                    // then
-                    exception.message shouldBe "Failed to create r_user_village"
+                    // when, then
+                    shouldThrowExactly<ExposedSQLException> {
+                        rUserVillageRepository.save(anotherUserId, villageId)
+                    }
                 }
                 it("VillageTableに存在しない村にユーザが参加すると例外が発生する") {
                     // given
-                    val userId = UserId(UUID.fromString(EXIST_USER_ID_STRING))
+                    val userId = UserId(UUID.randomUUID())
+                    val user = User(userId, "user", true)
                     val villageId = VillageId(UUID.randomUUID())
 
-                    // when, then
-                    val exception = shouldThrow<RuntimeException> {
-                        rUserVillageRepository.save(userId, villageId)
+                    // and
+                    transaction {
+                        UserTable.insert {
+                            it[id] = userId.value
+                            it[name] = user.name
+                            it[isActive] = user.isActive
+                        }
                     }
 
-                    // then
-                    exception.message shouldBe "Failed to create r_user_village"
+                    // when, then
+                    shouldThrowExactly<ExposedSQLException> {
+                        rUserVillageRepository.save(userId, villageId)
+                    }
                 }
             }
         }
