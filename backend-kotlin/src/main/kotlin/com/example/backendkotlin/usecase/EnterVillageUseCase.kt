1559d4c1171943142c56f8cbdaa7399f2df40d82
soiloof <tsucchy0718@gmail.com>
2025-03-02 20:32:47 +0900

    EnterVillageUseCaseを実装
    verifyを追加
    
    GrpcService.EnterVillageのリクエストに不備がある時の例外処理を追加
    
    org.springframework.data:spring-data-rest-webmvc
    
    人狼ゲーム用の例外クラスを作成
    
    村ログイン処理の例外処理を細分化
    
    例外クラスにcauseを追加
    
    ResourceNotFoundは自作クラスで対応するためdependencyから削除
    
    ResourceNotFoundExceptionを自作
    
    ErrorHandlingを共通して行うGrpcServiceExceptionHandlerを実装
    
    villageIdStringをvillageIdから生成するよう修正
    
    afterTestを修正
    
    ジェネリック型にProtobuf型の制約を追加

--- backend-kotlin/src/main/kotlin/com/example/backendkotlin/usecase/EnterVillageUseCase.kt
+++ backend-kotlin/src/main/kotlin/com/example/backendkotlin/usecase/EnterVillageUseCase.kt
@@ -7,9 +7,9 @@ import com.example.backendkotlin.domain.UserId
 import com.example.backendkotlin.domain.UserRepository
 import com.example.backendkotlin.domain.VillageId
 import com.example.backendkotlin.domain.VillageRepository
-import com.example.backendkotlin.infrastructure.db.table.RUserVillageTable.userId
+import com.example.backendkotlin.domain.WerewolfErrorCode
+import com.example.backendkotlin.domain.WerewolfException
 import org.springframework.stereotype.Service
-import java.util.UUID
 
 /**
  *  一般ユーザーが村にログインするユースケース
@@ -31,14 +31,14 @@ class EnterVillageUseCase(
      *
      * @return 村IDとユーザーID
      */
-    fun invoke(villageIdString: String, villagePassword: String, userName: String, userPassword: String): Pair<VillageId, UserId> {
+    fun invoke(villageIdString: String, villagePassword: String, userName: String, userPassword: String): Pair<UserId, VillageId> {
         // リクエストされた村が存在するか確認
-        val villageId = VillageId(UUID.fromString(villageIdString))
-        val villageAndHashedPassword = villageRepository.selectVillageById(villageId) ?: throw IllegalArgumentException("村が存在しません")
+        val villageId = VillageId.generate(villageIdString)
+        val villageAndHashedPassword = villageRepository.selectVillageById(villageId) ?: throw WerewolfException(WerewolfErrorCode.RESOURCE_NOT_FOUND, "村が存在しません")
 
         // 取得した村のパスワードが正しいか確認
-        if (HashedPassword.doesMatch(villagePassword, villageAndHashedPassword.second)) {
-            throw IllegalArgumentException("パスワードが違います")
+        if (!HashedPassword.doesMatch(villagePassword, villageAndHashedPassword.second)) {
+            throw WerewolfException(WerewolfErrorCode.VILLAGE_PASSWORD_IS_WRONG, "村のパスワードが違います")
         }
 
         // ユーザーを作成
@@ -52,9 +52,9 @@ class EnterVillageUseCase(
         userRepository.createUser(newUser, userHashedPassword)
 
         // ユーザーと村を紐付ける
-        rUserVillageRepository.save(userId, villageId)
+        val result = rUserVillageRepository.save(userId, villageId)
 
         // レスポンスを作成
-        return Pair(villageId, userId)
+        return result
     }
 }
