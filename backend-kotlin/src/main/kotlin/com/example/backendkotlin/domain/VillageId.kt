c18fb2b1e2a500b3b9a9e7e44b9687619e5c3582
soiloof <tsucchy0718@gmail.com>
2025-03-02 20:31:35 +0900

    文字列からVillageIdを生成できるよう修正

--- backend-kotlin/src/main/kotlin/com/example/backendkotlin/domain/VillageId.kt
+++ backend-kotlin/src/main/kotlin/com/example/backendkotlin/domain/VillageId.kt
@@ -4,8 +4,23 @@ import java.util.UUID
 
 data class VillageId(val value: UUID) {
     companion object {
+        /**
+         * 村IDを生成する
+         *
+         * @return 村ID
+         */
         fun generate(): VillageId {
             return VillageId(UUID.randomUUID())
+        }
+
+        /**
+         * 村IDを生成する
+         *
+         * @param value 村IDの文字列
+         * @return 村ID
+         */
+        fun generate(value: String): VillageId {
+            return VillageId(UUID.fromString(value))
         }
     }
 }
