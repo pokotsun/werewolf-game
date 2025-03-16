9090fd3c6b2aecd74521a2511ff8fb59f80d3eb8
soiloof <tsucchy0718@gmail.com>
2025-03-16 17:39:59 +0900

    UserIdに文字列から生成する関数を追加

--- backend-kotlin/src/test/kotlin/com/example/backendkotlin/domain/UserIdUT.kt
+++ backend-kotlin/src/test/kotlin/com/example/backendkotlin/domain/UserIdUT.kt
@@ -1,6 +1,7 @@
 package com.example.backendkotlin.domain
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import java.util.UUID
 
@@ -25,6 +26,26 @@ class UserIdUT : DescribeSpec() {
                 // when, then
                 shouldNotThrowAny {
                     UserId.generate()
+                }
+            }
+        }
+        describe("generate(value: String)") {
+            it("UUIDの仕様に沿った文字列からオブジェクトを生成できる") {
+                // given
+                val value = "123e4567-e89b-12d3-a456-426614174000"
+
+                // when, then
+                shouldNotThrowAny {
+                    UserId.generate(value)
+                }
+            }
+            it("UUIDの仕様に沿っていない文字列からオブジェクトを生成できない") {
+                // given
+                val value = "value"
+
+                // when, then
+                shouldThrow<IllegalArgumentException> {
+                    UserId.generate(value)
                 }
             }
         }
