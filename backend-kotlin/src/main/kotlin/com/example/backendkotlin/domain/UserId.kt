75fa7a2bd6a60f101e4c993da7ce04fe6d1cbbbc
soiloof <tsucchy0718@gmail.com>
2025-03-16 17:39:59 +0900

    UserIdに文字列から生成する関数を追加

--- backend-kotlin/src/main/kotlin/com/example/backendkotlin/domain/UserId.kt
+++ backend-kotlin/src/main/kotlin/com/example/backendkotlin/domain/UserId.kt
@@ -4,8 +4,23 @@ import java.util.UUID
 
 data class UserId(val value: UUID) {
     companion object {
+        /**
+         * ユーザーIDを生成する
+         *
+         * @return ユーザーID
+         */
         fun generate(): UserId {
             return UserId(UUID.randomUUID())
+        }
+
+        /**
+         * 文字列からユーザーIDを生成する
+         *
+         * @param userIdString ユーザーIDの文字列
+         * @return ユーザーID
+         */
+        fun generate(userIdString: String): UserId {
+            return UserId(UUID.fromString(userIdString))
         }
     }
 }
