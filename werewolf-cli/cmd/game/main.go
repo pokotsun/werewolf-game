2f5e645c7697ed35528786a1b670e9a7ce50f811
pokotsun <gorigolila@gmail.com>
2025-03-23 23:20:28 +0900

    ui モジュールに移動させた

--- werewolf-cli/cmd/game/main.go
+++ werewolf-cli/cmd/game/main.go
@@ -3,29 +3,13 @@ package main
 import (
 	"fmt"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/pokotsun/werewolf-game/ui"
 	"os"
 )
 
-type Model struct{}
-
-func (m Model) Init() tea.Cmd {
-	return tea.SetWindowTitle("Werewolf Game CLI")
-}
-
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg.(type) {
-	case tea.KeyMsg:
-		return m, tea.Quit
-	}
-	return m, nil
-}
-
-func (m Model) View() string {
-	return "\nPress any key to quit."
-}
-
 func main() {
-	if _, err := tea.NewProgram(Model{}).Run(); err != nil {
+	uiModel := ui.Model{}
+	if _, err := tea.NewProgram(uiModel).Run(); err != nil {
 		fmt.Printf("error starting program: %v\n", err)
 		os.Exit(1)
 	}
