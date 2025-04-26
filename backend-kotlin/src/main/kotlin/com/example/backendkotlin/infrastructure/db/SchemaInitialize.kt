f0813a0afa060a61f209f50d1f53c2966f391a70
soiloof <tsucchy0718@gmail.com>
2025-04-27 08:29:25 +0900

    migration
    migration spotlessAppply

--- backend-kotlin/src/main/kotlin/com/example/backendkotlin/infrastructure/db/SchemaInitialize.kt
+++ backend-kotlin/src/main/kotlin/com/example/backendkotlin/infrastructure/db/SchemaInitialize.kt
@@ -1,8 +1,14 @@
 package com.example.backendkotlin.infrastructure.db
 
+import com.example.backendkotlin.infrastructure.db.table.ActionLogTable
+import com.example.backendkotlin.infrastructure.db.table.BitingResultLogTable
+import com.example.backendkotlin.infrastructure.db.table.GameStatusTable
+import com.example.backendkotlin.infrastructure.db.table.GameTable
+import com.example.backendkotlin.infrastructure.db.table.PlayerTable
 import com.example.backendkotlin.infrastructure.db.table.RUserVillageTable
 import com.example.backendkotlin.infrastructure.db.table.UserTable
 import com.example.backendkotlin.infrastructure.db.table.VillageTable
+import com.example.backendkotlin.infrastructure.db.table.VotingResultLogTable
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
@@ -13,6 +19,16 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional
 class SchemaInitialize : ApplicationRunner {
     override fun run(args: ApplicationArguments) {
-        SchemaUtils.create(UserTable, VillageTable, RUserVillageTable)
+        SchemaUtils.create(
+            UserTable,
+            VillageTable,
+            RUserVillageTable,
+            GameTable,
+            GameStatusTable,
+            PlayerTable,
+            ActionLogTable,
+            BitingResultLogTable,
+            VotingResultLogTable,
+        )
     }
 }
